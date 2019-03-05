7520bf492e8e265a51b0c233de2ff7c827c11a0c
rafaella <rafaella2805@gmail.com>
2019-03-05 13:42:53 -0300

    Update main.js
    add new fuctions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// Funcao para criar tooltips de informacao
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+})
+// Funcao para chamar modal de historico escolar
+$('#modalHistorico').on('shown.bs.modal', function () {
+  $('#historico-escolar-modal').trigger('focus')
+})
+// Funcao para chamar modal de resumo projeto
+$('#modalResumo').on('shown.bs.modal', function () {
+  $('#resumo-projeto-modal').trigger('focus')
 })
