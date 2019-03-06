5a5adb37fe623fa997e82fadd81b4f0ad460963b
rafaella <rafaella2805@gmail.com>
2019-03-06 09:02:50 -0300

    new change

--- js/main.js
+++ js/main.js
@@ -2,11 +2,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-// Funcao para chamar modal de historico escolar
-$('#modalHistorico').on('shown.bs.modal', function () {
-  $('#historico-escolar-modal').trigger('focus')
-})
-// Funcao para chamar modal de resumo projeto
-$('#modalResumo').on('shown.bs.modal', function () {
-  $('#resumo-projeto-modal').trigger('focus')
-})
